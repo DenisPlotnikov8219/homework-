372906f253932f7421a672c627f51c95f82d3442
DenisPlotnikov8219 <146652876+DenisPlotnikov8219@users.noreply.github.com>
2026-05-29 09:55:59 +0300

    Add files via upload

---
+++ Плотников.py
@@ -0,0 +1,26 @@
+#1 задание
+a=int(input("ввкдите 1 число: "))
+b=int(input("ввкдите 2 число: "))
+c=int(input("ввкдите 3 число: "))
+print(f"{a+b+c}")
+print(f"{a*b*c}")
+
+#2 задание
+a=int(input("Зарплата за месяц: "))
+b=int(input("Сумма месячного платежа по кредиту в банке: "))
+c=int(input("Задолженность за коммунальные услуги: "))
+print(f"{a-b-c}")
+
+#3 задание
+a=int(input("Введите длину первой диагонали:"))
+b=int(input("Введите длину второй диагонали:"))
+print(f"Площадь ромба: {a*b/2}")
+
+#4 задание
+print("To be\nor not\nto be")
+
+#5 задание
+print("Life is what happens")
+print("\twhen")
+print("\t\tyou're busy making other plans")
+print("\t\t\t\t\t\tJohn Lennon")
